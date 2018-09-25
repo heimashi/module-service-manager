8d18596b29e44324a17c2b90d664919c254fd575
shiwang <shiwang@rong360.com>
2018-09-25 15:26:44 +0800

    update modules compile

--- sample/app/src/main/java/com/rong360/example/ExampleActivity.kt
+++ sample/app/src/main/java/com/rong360/example/ExampleActivity.kt
@@ -3,9 +3,6 @@ package com.rong360.example
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.rong360.example.a.*
-import com.rong360.example.b.*
-import com.rong360.msm.api.ModuleServiceManager
 import kotlinx.android.synthetic.main.app_activity_example.*
 
 class ExampleActivity : AppCompatActivity() {
@@ -13,16 +10,14 @@ class ExampleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.app_activity_example)
-        //initModules()
         toAModule.setOnClickListener {
-            startActivity(Intent(this, AModuleActivity::class.java))
+            //Test code. Using router for activity is better.
+            startActivity(Intent().setClassName(this@ExampleActivity, "com.rong360.example.a.AModuleActivity"))
         }
         toBModule.setOnClickListener{
-            startActivity(Intent(this, BModuleActivity::class.java))
+            //Test code. Using router for activity is better.
+            startActivity(Intent().setClassName(this@ExampleActivity, "com.rong360.example.b.BModuleActivity"))
         }
     }
 
-    private fun initModules() {
-        //ModuleServiceManager.instance.registerModules(AModuleIndex(), BModuleIndex())
-    }
 }
